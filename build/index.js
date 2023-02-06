1971d8e40f5f067b8c1a30c8f53e956ee0d5b617
TrueSunGaming <truesungaming@gmail.com>
2023-02-05 20:39:22 -0500

    update ts target to newer js

--- build/index.js
+++ build/index.js
@@ -1,23 +1,14 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    const s = "Tristable Company";
+"use strict";
+(() => {
     const el = document.querySelector("#title");
-    let i = 0;
     if (!el)
         return;
+    const s = el.innerHTML;
+    let i = 0;
     const interval = setInterval(() => {
         i++;
         if (i > s.length)
             return clearInterval(interval);
         el.innerHTML = s.substring(0, i) + (i != s.length ? "|" : "");
     }, 100);
-}))();
-export {};
+})();
