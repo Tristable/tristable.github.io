8d89d7cfc2d49eaa01e6e396b907683bbf2baf95
TrueSunGaming <truesungaming@gmail.com>
2023-02-05 20:37:21 -0500

    title screen done

---
+++ index.ts
@@ -0,0 +1,11 @@
+(() => {
+    const el = document.querySelector("#title");
+    if (!el) return;
+    const s = el.innerHTML;
+    let i = 0;
+    const interval = setInterval(() => {
+        i++;
+        if (i > s.length) return clearInterval(interval);
+        el.innerHTML = s.substring(0, i) + (i != s.length ? "|" : "");
+    }, 100);
+})();
